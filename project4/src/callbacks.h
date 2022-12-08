80ac8fe920407fc0a56fbd11ae1cba8404602913
Louay-Esprit <louay.mehrez@esprit.tn>
2022-12-08 19:09:25 +0100

    '''''''''

---
+++ project4/src/callbacks.h
@@ -0,0 +1,10 @@
+#include <gtk/gtk.h>
+
+
+void
+on_btnfiltrage_clicked                 (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_btnfiltrage2_clicked                (GtkWidget       *button,
+                                        gpointer         user_data);
