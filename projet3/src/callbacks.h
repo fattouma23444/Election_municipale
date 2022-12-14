a3be8b43b406a4ddd9db0adcd2f64b4672c55ea1
Mhmariem <115408486+Mhmariem@users.noreply.github.com>
2022-12-14 23:21:07 +0100

    Add files via upload
    statistique

---
+++ projet3/src/callbacks.h
@@ -0,0 +1,30 @@
+#include <gtk/gtk.h>
+
+
+void
+on_buttonSTAT1_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonSTAT2_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonSTAT3_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonSTAT4_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonSTAT5_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonSTAT6_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonretourSTAT_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
